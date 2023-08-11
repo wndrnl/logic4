c1ec02854e6a9be266e45dd688106919a89f134d
Len van Essen <len@wndr.digital>
2023-08-11 14:57:09 +0200

    Add warehouse request

--- src/Requests/Stock/GetWarehousesStockLocationsForAdministration.php
+++ src/Requests/Stock/GetWarehousesStockLocationsForAdministration.php
@@ -1,13 +1,29 @@
 <?php
 
+
 namespace Wndr\Logic4\Requests\Stock;
 
-use Wndr\Logic4\Requests\Abstracts\PostRequest;
-
-class GetWarehousesStockLocationsForAdministration extends PostRequest
+use Saloon\Enums\Method;
+use Saloon\Traits\Body\HasBody;
+use Saloon\Http\Request;
+use Saloon\Contracts\Body\HasBody as HasBodyContract;
+class GetWarehousesStockLocationsForAdministration extends Request implements HasBodyContract
 {
+    use HasBody;
+
+    protected Method $method = Method::POST;
+
+    public function __construct(
+        protected int $administrationId
+    ) {}
+
     public function resolveEndpoint(): string
     {
         return '/v1/Stock/GetWarehousesStockLocationsForAdministration';
+    }
+
+    public function defaultBody(): string
+    {
+        return (string)$this->administrationId;
     }
 }
