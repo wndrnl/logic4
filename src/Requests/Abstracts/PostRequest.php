850d9409e23c65d95248743c8d37f4fd5c9901cb
Len van Essen <len@wndr.digital>
2023-08-11 10:43:50 +0200

    Allow different bodies

--- src/Requests/Abstracts/PostRequest.php
+++ src/Requests/Abstracts/PostRequest.php
@@ -12,7 +12,7 @@ abstract class PostRequest extends Request implements HasBody
     use HasJsonBody;
     protected Method $method = Method::POST;
     public function __construct(
-        protected array $b
+        protected mixed $b
     ){}
 
     protected function defaultBody(): array
